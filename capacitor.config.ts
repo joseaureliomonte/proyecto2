d6288c1656bc127cb0bfd0eecf320c68d8cfb833
joseaureliomonte <joseaureliomontesoto@alumno.ieselrincon.es>
2024-01-24 21:20:11 +0000

    Este es un commit inicial de un un proyecto prueba

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.proyecto2',
+  appName: 'proyecto2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
